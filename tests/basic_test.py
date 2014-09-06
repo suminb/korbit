121d7b7bc8c183fa06efa4bea30cff358209bec6
Sumin Byeon <suminb@gmail.com>
2014-09-07 00:31:30 +0900

    More test cases for the orderbook API call

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -1,20 +1,32 @@
-from __future__ import absolute_import  
+from __future__ import absolute_import
 from korbit.api import *
 
 
 def test_get_orderbook():
-	orderbook = get_orderbook()
+    orderbook = get_orderbook()
 
-	assert 'timestamp' in orderbook
-	assert 'bids' in orderbook
-	assert 'asks' in orderbook
+    assert 'timestamp' in orderbook
+    assert 'bids' in orderbook
+    assert 'asks' in orderbook
+
+
+def test_get_orderbook_by_type():
+    orderbook_bids = get_orderbook('bids')
+
+    # NOTE: Is this enough?
+    assert isinstance(orderbook_bids, list)
+
+    orderbook_asks = get_orderbook('asks')
+
+    # NOTE: Is this enough?
+    assert isinstance(orderbook_asks, list)
 
 
 def test_get_user_info():
-	info = get_user_info()
+    info = get_user_info()
 
-	assert 'email' in info
-	assert 'prefs' in info and type(info['prefs']) == dict
+    assert 'email' in info
+    assert 'prefs' in info and type(info['prefs']) == dict
 
 
 def test_buying_order():
